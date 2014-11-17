e9133ddfd6e28140024c1755e740c7f1cbf8525f
Marcos Portillo <marcosportillo111@gmail.com>
2014-11-17 13:34:04 -0600

    Create a web controller

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by marcosportillo on 17/11/14.
+ */
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model){
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
